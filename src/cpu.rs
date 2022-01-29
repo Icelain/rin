f00357ec32dcbb306b4d03479761f1738067974a
Icelain <xerneas965@gmail.com>
2022-01-29 19:38:52 +0530

    wrote stable logic for cpu.rs and did some refactoring in ram.rs

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,18 +1,15 @@
 use std::fs;
+use std::collections::HashMap;
 
 #[derive(Debug, Default)]
 pub struct CpuInfo {
-    pub cache_size: usize,
+    pub cache_size: String,
     pub cores: usize,
-    pub cpu_mhz: f32,
+    pub cpu_speed: String,
     pub model_name: String,
     pub vendor_id: String,
     pub is_fpu: bool,
-    pub cpuid_level: f32,
-}
-
-fn parse_str(data: &str) -> &str {
-    data.split(":").nth(1).unwrap().trim()
+    pub cpuid_level: usize,
 }
 
 impl CpuInfo {
@@ -25,35 +22,67 @@ impl CpuInfo {
     pub fn fetch(&mut self) {
         let raw_data =
             fs::read_to_string("/proc/cpuinfo").expect("unable to read from cpuinfo file");
+        
+        let mut map = HashMap::new();
 
-        let mut data = raw_data.lines();
+        for (i, line) in raw_data.lines().enumerate() {
+            
+            if i >= 20 {
 
-        let vendor_id = parse_str(data.nth(1).unwrap()).to_string();
-        let model_name = parse_str(data.nth(4).unwrap()).to_string();
+                break;
 
-        let cpuid_level = parse_str(data.nth(10).unwrap()).parse::<f32>().unwrap();
-        let cpu_cores = parse_str(data.nth(12).unwrap()).parse::<usize>().unwrap();
+            }
 
-        let cache_size = parse_str(data.nth(8).unwrap())
-            .trim_end_matches("KB")
+            let kv: Vec<&str> = line.split(":").collect();
+
+            map.insert(kv[0].trim(), kv[1].trim());
+
+        }
+        
+        let vendor_id = map.get("vendor_id")
+            .expect("could not retrieve vendor id")
+            .trim()
+            .to_string();
+        
+        let model_name = map.get("model name")
+            .expect("could not retrieve model name") 
+            .trim()
+            .to_string();
+
+        let cpu_cores = map.get("cpu cores")
+            .expect("could not retrieve cpu cores")
             .trim()
             .parse::<usize>()
             .unwrap();
 
-        let cpu_mhz = parse_str(data.nth(8).unwrap()).parse::<f32>().unwrap();
+        let cpuid_level = map.get("cpuid level")
+            .expect("could not retrieve cpuid level")
+            .trim()
+            .parse::<usize>()
+            .unwrap();
 
-        let is_fpu = if parse_str(data.nth(15).unwrap()) == "yes" {
-            true
-        } else {
-            false
-        };
+        let cache_size = map.get("cache size")
+            .expect("could not retrieve cache size")
+            .trim()
+            .to_string();
+
+        let is_fpu = if map.get("fpu")
+            .expect("could not retrieve fpu")
+            .trim() == "yes" {true} else {false};
+
+        let cpu_ghz = model_name
+            .split("@")
+            .nth(1)
+            .unwrap()
+            .trim()
+            .to_string();
 
         self.vendor_id = vendor_id;
         self.model_name = model_name;
         self.cpuid_level = cpuid_level;
         self.cores = cpu_cores;
         self.cache_size = cache_size;
-        self.cpu_mhz = cpu_mhz;
+        self.cpu_speed = cpu_ghz;
         self.is_fpu = is_fpu;
     }
 }
--- tests/cpu.rs
+++ tests/cpu.rs
@@ -8,11 +8,13 @@ mod cpu {
         let mut c = CpuInfo::new();
 
         c.fetch();
+        
+        println!("{:?}", c);
 
         assert_eq!(c.cores > 0, true);
         assert_eq!(c.model_name.as_str() != "", true);
         assert_eq!(c.vendor_id.as_str() != "", true);
-        assert_eq!(c.cpu_mhz > 0., true);
+        assert_eq!(c.cpu_speed.replace("GHz", "").parse::<f32>().unwrap() >= 0. , true);
 
     } 
     
