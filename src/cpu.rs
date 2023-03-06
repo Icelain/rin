6a228022b1c4812bb71260dbedef50a80d23a269
icelain <xerneas965@gmail.com>
2023-03-06 23:23:24 +0530

    fixed cpu.rs panic and cpu speed

--- src/cpu.rs
+++ src/cpu.rs
@@ -5,7 +5,8 @@ use std::collections::HashMap;
 pub struct CpuInfo {
     pub cache_size: String,
     pub cores: usize,
-    pub cpu_speed: String,
+    // threadwise cpu_mhz for most precision
+    pub cpu_speed: Vec<(usize, f64)>,
     pub model_name: String,
     pub vendor_id: String,
     pub is_fpu: bool,
@@ -20,22 +21,44 @@ impl CpuInfo {
     }
 
     pub fn fetch(&mut self) {
+
         let raw_data =
             fs::read_to_string("/proc/cpuinfo").expect("unable to read from cpuinfo file");
         
+        // hashmaps are used so that the code works even after changes in the cpuinfo file
+        // vectors would not have been able to keep track of the index of keys properly
         let mut map = HashMap::new();
+        
+        // cpu_mhz_tracker keeps track of the cpu_mhz at each iteration of the loop as well as the
+        // number of iterations
+        let mut cpu_mhz_tracker: Vec<(usize, f64)> = vec![];
+        
+        // l keeps track of no of threads
+        let mut l = 0;
 
-        for (i, line) in raw_data.lines().enumerate() {
+        for (_, line) in raw_data.lines().enumerate() {
             
-            if i >= 20 {
-
-                break;
-
+            if !line.contains(":") {
+                
+                continue;
+    
             }
 
             let kv: Vec<&str> = line.split(":").collect();
+            
+            let key = kv[0].trim();
+            let value = kv[1].trim();
+            
+            if key == "cpu MHz" { 
 
-            map.insert(kv[0].trim(), kv[1].trim());
+                l += 1;
+                cpu_mhz_tracker.push((l,value.parse::<f64>().unwrap())); 
+
+                continue
+    
+            }
+
+            map.insert(key, value);
 
         }
         
@@ -70,19 +93,12 @@ impl CpuInfo {
             .expect("could not retrieve fpu")
             .trim() == "yes" {true} else {false};
 
-        let cpu_ghz = model_name
-            .split("@")
-            .nth(1)
-            .unwrap()
-            .trim()
-            .to_string();
-
         self.vendor_id = vendor_id;
         self.model_name = model_name;
         self.cpuid_level = cpuid_level;
         self.cores = cpu_cores;
         self.cache_size = cache_size;
-        self.cpu_speed = cpu_ghz;
+        self.cpu_speed = cpu_mhz_tracker;
         self.is_fpu = is_fpu;
     }
 }
